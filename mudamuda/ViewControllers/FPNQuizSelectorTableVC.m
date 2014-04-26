2b920f809188f5180cf313d91558c677e4a8ce34
htsang <hentsang@gmail.com>
2014-04-25 23:28:45 -0400

    more kana plists

--- mudamuda/ViewControllers/FPNQuizSelectorTableVC.m
+++ mudamuda/ViewControllers/FPNQuizSelectorTableVC.m
@@ -34,7 +34,7 @@
     self.quizGenerator = @[[[FPNAllRomanjiForKana alloc] init]];
     
     // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+    self.clearsSelectionOnViewWillAppear = YES;
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;

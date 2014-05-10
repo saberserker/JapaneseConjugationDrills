e299d0420553a6924e0b0407b858985114d2ef88
htsang <hentsang@gmail.com>
2014-05-10 15:42:49 -0400

    rudimentary test for conjugation method. This tests all of the verbs on all the conjugations to make sure they are not equal for some stupid reason.

--- mudamudaTests/mudamudaTests.m
+++ mudamudaTests/mudamudaTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "FPNConjugateJapaneseQuizGenerator.h"
 
 @interface mudamudaTests : XCTestCase
 
@@ -24,6 +25,36 @@
 {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+}
+
+- (void)testForConjugationInequity {
+    NSArray* withAdj = @[@"V1",@"V5",@"ADJNA",@"ADJI"];
+    NSArray* noAdj = @[@"V1",@"V5"];
+    NSDictionary* rules = @{@"TEFORM":withAdj,
+                            @"TAFORM":withAdj,
+                            @"NAIFORM":withAdj,
+                            @"IFORM":noAdj,
+                            @"ERUFORM":noAdj,
+                            @"CAUSATIVEFORM":withAdj,
+                            @"PASSIVEFORM":noAdj,
+                            @"EBAFORM":withAdj,
+                            @"IMPERATIVEFORM":noAdj,
+                            @"VOLITIONALFORM":withAdj,};
+    
+    NSDictionary* vocab = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"japanese_words" ofType:@"plist"]];
+    
+    FPNConjugateJapaneseQuizGenerator* generator = [FPNConjugateJapaneseQuizGenerator new];
+    for (NSString* conjugation in [rules allKeys]) {
+        NSArray* validWordTypesForConjugation = rules[conjugation];
+        for (NSString* wordTypeKey in validWordTypesForConjugation) {
+            NSDictionary* wordsOfType = vocab[wordTypeKey];
+            for (NSString* word in [wordsOfType allKeys]) {
+                NSString* conjugatedWord = [generator conjugatePlainForm:word wordType:wordTypeKey conjugationType:conjugation];
+                XCTAssert(![word isEqualToString:conjugatedWord], @"Unconjugated Word should not be equal to plain version!");
+            }
+        }
+    }
+    
 }
 
 - (void)testExample
